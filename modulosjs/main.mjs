a5cb20b435632df22dffda84cf22df6c637483ee
Leticia Fanan de Barros <98134462+leticiafanan@users.noreply.github.com>
2022-07-10 17:30:26 -0300

    Add files via upload

---
+++ modulosjs/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Letícia', 23));
+console.log(mostraCidade('Letícia', 'Franca'));
+console.log(mostraHobby('Letícia', 'ler livros'));
